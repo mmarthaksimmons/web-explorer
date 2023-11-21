3917f8fef1fbded703699c7792e16b9649dd249b
mmarthaksimmons <90653901+mmarthaksimmons@users.noreply.github.com>
2023-11-21 21:52:26 +0800

    Update web_explorer.py

--- web_explorer.py
+++ web_explorer.py
@@ -5,10 +5,10 @@ from langchain.retrievers.web_research import WebResearchRetriever
 
 import os
 
-os.environ["GOOGLE_API_KEY"] = "YOUR_API_KEY" # Get it at https://console.cloud.google.com/apis/api/customsearch.googleapis.com/credentials
-os.environ["GOOGLE_CSE_ID"] = "YOUR_CSE_ID" # Get it at https://programmablesearchengine.google.com/
+os.environ["GOOGLE_API_KEY"] = "AIzaSyANu41ws9ISdMaLRDN47HvGC3qF6DsX9cU" # Get it at https://console.cloud.google.com/apis/api/customsearch.googleapis.com/credentials
+os.environ["GOOGLE_CSE_ID"] = "f12c29c6a0cc74ce0" # Get it at https://programmablesearchengine.google.com/
 os.environ["OPENAI_API_BASE"] = "https://api.openai.com/v1"
-os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY" # Get it at https://beta.openai.com/account/api-keys
+os.environ["OPENAI_API_KEY"] = "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" # Get it at https://beta.openai.com/account/api-keys
 
 st.set_page_config(page_title="Interweb Explorer", page_icon="🌐")
 
