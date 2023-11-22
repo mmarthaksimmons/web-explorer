741d71776595c7dc697126d2067bb78db45d8ee2
mmarthaksimmons <90653901+mmarthaksimmons@users.noreply.github.com>
2023-11-22 12:38:42 +0800

    Update web_explorer.py

--- web_explorer.py
+++ web_explorer.py
@@ -8,7 +8,7 @@ import os
 os.environ["GOOGLE_API_KEY"] = "AIzaSyANu41ws9ISdMaLRDN47HvGC3qF6DsX9cU" # Get it at https://console.cloud.google.com/apis/api/customsearch.googleapis.com/credentials
 os.environ["GOOGLE_CSE_ID"] = "f12c29c6a0cc74ce0" # Get it at https://programmablesearchengine.google.com/
 os.environ["OPENAI_API_BASE"] = "https://api.openai.com/v1"
-os.environ["OPENAI_API_KEY"] = "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" # Get it at https://beta.openai.com/account/api-keys
+os.environ["OPENAI_API_KEY"] = "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" # Get it at https://beta.openai.com/account/api-keys
 
 st.set_page_config(page_title="Interweb Explorer", page_icon="🌐")
 
