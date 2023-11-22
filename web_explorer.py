606c9c4d3e135772fc9ee817413c14dfeb5aaf25
mmarthaksimmons <90653901+mmarthaksimmons@users.noreply.github.com>
2023-11-22 10:40:11 +0800

    Update web_explorer.py

--- web_explorer.py
+++ web_explorer.py
@@ -26,7 +26,7 @@ def settings():
 
     # LLM
     from langchain.chat_models import ChatOpenAI
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0, streaming=True)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True)
 
     # Search
     from langchain.utilities import GoogleSearchAPIWrapper
